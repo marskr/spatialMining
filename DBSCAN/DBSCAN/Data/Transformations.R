a7ce7fb49d8140b9aa5c894f03aad1258926e0a2
marskr <marskr@opoczta.pl>
2018-12-09 13:31:23 +0100

    tiny fix #1

--- DBSCAN/DBSCAN/Data/Transformations.R
+++ DBSCAN/DBSCAN/Data/Transformations.R
@@ -1,4 +1,22 @@
 
+preprocessDataset <- function(D) {
+
+    idSeq = as.data.frame(seq(1, dim(D)[1], 1))
+
+    label = as.data.frame(rep(0, dim(D)[1]))
+
+    colnames(idSeq) = c("id")
+
+    colnames(label) = c("label")
+
+    D = cbind(idSeq, D)
+
+    D = cbind(D, label)
+
+    D
+}
+
+
 inc <- function(x) {
 
     eval.parent(substitute(x <- x + 1))
--- DBSCAN/DBSCAN/Main.R
+++ DBSCAN/DBSCAN/Main.R
@@ -1,25 +1,13 @@
 library(geosphere)
-library(rlist)
 
 source("Data/ConnectionToDB.R")
 source("Data/Transformations.R")
 source("Algorithm/Distance.R")
 source("Algorithm/DBSCAN.R")
 
-idSeq = as.data.frame(seq(1, dim(treesFrame)[1], 1))
-
-label = as.data.frame(rep(0, dim(treesFrame)[1]))
-
-colnames(idSeq) = c("id")
-
-colnames(label) = c("label")
-
-treesFrame = cbind(idSeq, treesFrame)
-
-treesFrame = cbind(treesFrame, label)
+treesFrame = preprocessDataset(treesFrame)
 
 eps = 0.01
 minPts = 2
 
 DBSCAN(treesFrame, eps, minPts)
-
